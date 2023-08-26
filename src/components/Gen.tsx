e37cf8421cb04ba2b2619a98fd823974c03d769a
DM <danymartinezluna@gmail.com>
2023-08-26 10:45:59 -0400

    Added text to image

--- src/components/Gen.tsx
+++ src/components/Gen.tsx
@@ -4,11 +4,22 @@ import { useState } from 'react';
 const Gen = () => {
   const [meme, setMeme] = useState({ topText: '', bottomText: '', url: '' });
   const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+
   const getMemeImage = () => {
     const memeArray = allMemeImages.data.memes;
     const randomMeme = memeArray[Math.floor(Math.random() * memeArray.length)];
-    setMeme((prevMeme) => ({ ...prevMeme, url: randomMeme.url }));
+    
+    setMeme((prevMeme) => ({ ...prevMeme, url: randomMeme.url}));
   };
+
+  const getText= (event:{ target:{} }) => {
+    const { name , value, checked, type } = event.target;
+    console.log( typeof name)
+    setMeme((prevMeme)=>({...prevMeme, [name]: type === 'checked' ? checked : value }));
+  };
+
+
 
   return (
     <div className="p-9">
@@ -17,13 +28,19 @@ const Gen = () => {
           type="text"
           placeholder="Top Text"
           className="border-2 border-gray-700 rounded-xl p-1 pr-12 text-2xl text-center drop-shadow-lg
-        "
+          "
+          name="topText"
+          value={meme.topText}
+          onChange={getText}
         />
         <input
           type="text"
           placeholder="Bottom Text"
           className="border-2 border-gray-700 rounded-xl p-1 pr-12 text-2xl text-center drop-shadow-lg
         "
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={getText}
         />
 
         <button
@@ -36,10 +53,10 @@ const Gen = () => {
       <div className="absolute">
         <img src={meme.url} className=" rounded max-w-full mt-6 " />
         <h2 className="absolute w-[80%] text-center left-[50%]  py-1 font-impact text-[2em] uppercase text-white top-8 -translate-x-[50%]  ">
-          Top Text
+          {meme.topText}
         </h2>
         <h2 className="absolute w-[80%] text-center left-[50%]  py-1 font-impact text-[2em] uppercase text-white bottom-4 -translate-x-[50%]">
-          Bottom Text
+          {meme.bottomText}
         </h2>
       </div>
     </div>
